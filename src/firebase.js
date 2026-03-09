a98257968ee7cdd81b4b3d9c34a43dc2e6d79e7b
1510Darshan <Darshanwalhe1510@gmail.com>
2026-03-10 00:24:46 +0530

    Fixed Secret key leak

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import "dotenv/config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7kutjWs1BRddCrdxE6-q60HACSl4n9-Y",
-  authDomain: "sheild-bd2e1.firebaseapp.com",
-  projectId: "sheild-bd2e1",
-  storageBucket: "sheild-bd2e1.firebasestorage.app",
-  messagingSenderId: "315301563916",
-  appId: "1:315301563916:web:1ce97daeefe414c4eea829",
-  measurementId: "G-7L454H1EYX"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
