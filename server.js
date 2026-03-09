fff17718ff3203cdbc07e019dea233e0928e2d2c
1510Darshan <Darshanwalhe1510@gmail.com>
2026-03-10 01:05:28 +0530

    build fix

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { GoogleGenAI } from "@google/genai";
+import path from "path";
+
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "frontend/build")));
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
